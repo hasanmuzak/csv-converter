9b5b6074fbf6d3cfdf2688b50007629704c54076
Hasan Muzak <hasanmuzak@yandex.com>
2022-09-15 19:59:45 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum nesciunt beatae error ex facere iste esse, ea at magnam nulla est, a incidunt explicabo maxime inventore! Blanditiis iste repellat libero?
+    </div>
+  );
+}
+
+export default App;
